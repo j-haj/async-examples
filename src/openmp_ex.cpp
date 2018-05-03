f85d56cfb182d890102c827fd9009976d75e7d52
Jeff Hajewski <jeff.hajewski@gmail.com>
2018-05-02 21:04:07 -0500

    Adds openmp example

---
+++ src/openmp_ex.cpp
@@ -0,0 +1,90 @@
+#include <chrono>
+#include <cmath>
+#include <iostream>
+#include <random>
+#include <set>
+#include <queue>
+#include <time.h>
+#include <omp.h>
+
+constexpr unsigned int kSleepDuration = 1;
+
+template <typename T>
+std::ostream& operator<<(std::ostream& os, const std::queue<T>& q) {
+  std::queue<T> tmp(q);
+  os << "[ ";
+  while (!tmp.empty()) {
+    os << tmp.front() << ' ';
+    tmp.pop();
+  }
+  os << ']';
+  return os;
+}
+
+bool is_prime_slow(long long n) {
+  bool result = true;
+#pragma omp parallel for
+  for (long long i = 2; i < n; ++i) {
+    if (n % i == 0) result = false;
+  }
+  return result;
+}
+
+
+
+void random_sleep() {
+  static std::random_device rd;
+  static std::mt19937 gen(rd());
+  std::uniform_int_distribution<long> dist(10000, 1000000);
+  struct timespec tspec {tv_sec: static_cast<time_t>(kSleepDuration), tv_nsec: dist(gen)};
+  nanosleep(&tspec, NULL);
+}
+
+class Producer {
+  public:
+  Producer(long long low, long long high) : low_(low), high_(high) {}
+  long long generate() {
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    random_sleep();
+    std::uniform_int_distribution<long long> dist(low_, high_);
+    return dist(gen);
+  }
+
+  private:
+  long long low_;
+  long long high_;
+};
+
+class Worker {
+  public:
+  void work(long long n) {
+    if (is_prime_slow(n)) std::cout << n << " is prime" << std::endl;
+    else std::cout << n << " is not prime" << std::endl;
+  }
+};
+
+int main() {
+  using steady_clock = std::chrono::steady_clock;
+
+  auto start = steady_clock::now();
+
+  std::queue<long long> prime_q;
+  Producer producer(1000000, 100000000);
+  for (int i = 0; i < 10; i++) {
+    auto val = producer.generate();
+    std::cout << "Produced " << val << '\n';
+    prime_q.push(val);
+  }
+  std::cout << prime_q << '\n';
+  Worker worker;
+  while (!prime_q.empty()) {
+    worker.work(prime_q.front());
+    prime_q.pop();
+  }
+
+  auto stop = steady_clock::now();
+  std::chrono::duration<double> elapsed = stop - start;
+  std::cout << "Total elapsed time " << elapsed.count() << '\n';
+  return 0;
+}
